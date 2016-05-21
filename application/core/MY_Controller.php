c923d3ffd0298d54c57379da6c217743beb5e1b2
Alexandru Berce <alexandru.berce@gmail.com>
2016-05-21 05:25:25 +0300

    settings

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -10,7 +10,8 @@ class MY_Controller extends MY_Controller_Common {
 	//My Controller is used for Backed only
 	function __construct(){
 		parent::__construct();
-
 		$this->checkIfIsLoggedIn();
+		$this->load->model('Account_model');
+		$this->account_data = $this->Account_model->getAccountData();
 	}
 }
