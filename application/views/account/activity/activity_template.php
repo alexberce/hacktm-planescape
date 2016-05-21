01042754bb56c4400f37988e7f8d5a431d074df6
Alina Berce <Alina Berce>
2016-05-21 22:08:07 +0300

    added template

--- application/views/account/activity/activity_template.php
+++ application/views/account/activity/activity_template.php
@@ -1,21 +1,27 @@
-<div class="all-ended-event">
+
+<?php if($date == 'end_date') {
+    $class = 'ended';
+} else {
+    $class = 'upcoming';
+}?>
+<div class="all-<?php echo $class;?>-event" >
     <div class="ended-events-top">
         <img src="<?php echo  base_url() .$activity['path'] ; ?>">
     </div>
-    <div class="ended-events-bottom">
-        <div class="ended-events-date">
-            <div class="ended-events-date-day">
-                <?php echo intval(date("d", strtotime($activity['end_date']))); ?>
+    <div class="<?php echo $class;?>-events-bottom">
+        <div class="<?php echo $class;?>-events-date">
+            <div class="<?php echo $class;?>-events-date-day">
+                <?php echo intval(date("d", strtotime($date))); ?>
             </div>
-            <div class="ended-events-date-month">
-                <?php echo strtoupper(date("M", strtotime($activity['end_date']))); ?>
+            <div class="<?php echo $class;?>-events-date-month">
+                <?php echo strtoupper(date("M", strtotime($date))); ?>
             </div>
         </div>
-        <div class="ended-events-info">
-            <div class="ended-events-description">
+        <div class="<?php echo $class;?>-events-info">
+            <div class="<?php echo $class;?>-events-description">
                 <?php echo $activity['title']; ?>
             </div>
-            <div class="ended-events-votes">
+            <div class="<?php echo $class;?>-events-votes">
                 <i class="fa fa-lg fa-users" aria-hidden="true"></i>
                 <?php echo $activity['votes'].' Will be there.'; ?>
             </div>
--- application/views/account/activity/list_ended.php
+++ application/views/account/activity/list_ended.php
@@ -2,6 +2,6 @@
 <div class="all-ended-events">
     <?php $activities = isset($activities) ? $activities : array(); ?>
     <?php foreach ($activities as $activity) { ?>
-        <?php $this->load->view('account/activity/activity_template.php',array('activity' => $activity));?>
+        <?php $this->load->view('account/activity/activity_template.php',array('activity' => $activity,'date' => 'end_date'));?>
     <?php } ?>
 </div>
