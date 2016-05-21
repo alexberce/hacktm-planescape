64c29eae7c55b432c85c3269536976ba5775b204
Alexandru Berce <alexandru.berce@gmail.com>
2016-05-22 01:29:43 +0300

    renamed Ended with past

--- application/views/account/header.php
+++ application/views/account/header.php
@@ -82,7 +82,7 @@
 							</li>
 
 							<li><a href="<?=base_url()?>activity/ended">
-									<i class="fa fa-history"></i> Ended Events </a>
+									<i class="fa fa-history"></i> Past Events </a>
 							</li>
 
 							<li><a href="<?=base_url()?>invitation/add">
