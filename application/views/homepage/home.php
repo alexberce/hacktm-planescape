16990f2b2b18b789097ec19dd5207878d3fbe8fe
Colar Paul <colarpaul@gmail.com>
2016-05-22 01:19:51 +0300

    Improved Home view.

--- application/views/homepage/home.php
+++ application/views/homepage/home.php
@@ -13,7 +13,9 @@
 	<link href="<?=base_url()?>frontend-assets/css/bootstrap.min.css" rel="stylesheet">
 
 	<!-- Custom CSS -->
-	<link href="<?=base_url()?>frontend-assets/css/landing-page.css" rel="stylesheet">
+	<link href="<?=base_url()?>frontend-assets/css/
+
+	landing-page.css" rel="stylesheet">
 
 	<!-- Custom Fonts -->
 	<link href="<?=base_url()?>frontend-assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
@@ -30,205 +32,247 @@
 
 <body>
 
-<!-- Navigation -->
-<nav class="navbar navbar-default navbar-fixed-top topnav" role="navigation">
-	<div class="container topnav">
-		<!-- Brand and toggle get grouped for better mobile display -->
-		<div class="navbar-header">
-			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-			<a class="navbar-brand topnav" href="#">PlanEscape</a>
-		</div>
-		<!-- Collect the nav links, forms, and other content for toggling -->
-		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-			<ul class="nav navbar-nav navbar-right">
-				<li>
-					<a href="#about">About</a>
-				</li>
-				<li>
-					<a href="#services">Services</a>
-				</li>
-				<li>
-					<a href="#contact">Contact</a>
-				</li>
-				<li>
-					<a href="<?=base_url()?>account/login">Login</a>
-				</li>
-			</ul>
-		</div>
-		<!-- /.navbar-collapse -->
-	</div>
-	<!-- /.container -->
-</nav>
-
-
-<!-- Header -->
-<a name="about"></a>
-<div class="intro-header">
-	<div class="container">
-
-		<div class="row">
-			<div class="col-lg-12">
-				<div class="intro-message">
-					<h1>Landing Page</h1>
-					<h3>A Template by Start Bootstrap</h3>
-					<hr class="intro-divider">
-					<ul class="list-inline intro-social-buttons">
-						<li>
-							<a href="https://twitter.com/SBootstrap" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
-						</li>
-						<li>
-							<a href="https://github.com/IronSummitMedia/startbootstrap" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
-						</li>
-						<li>
-							<a href="#" class="btn btn-default btn-lg"><i class="fa fa-linkedin fa-fw"></i> <span class="network-name">Linkedin</span></a>
-						</li>
-					</ul>
-				</div>
+	<!-- Navigation -->
+	<nav class="navbar navbar-default navbar-fixed-top topnav" role="navigation">
+		<div class="container topnav">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand topnav" href="#">PlanEscape</a>
 			</div>
-		</div>
-
-	</div>
-	<!-- /.container -->
-
-</div>
-<!-- /.intro-header -->
-
-<!-- Page Content -->
-
-<a  name="services"></a>
-<div class="content-section-a">
-
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-5 col-sm-6">
-				<hr class="section-heading-spacer">
-				<div class="clearfix"></div>
-				<h2 class="section-heading">Death to the Stock Photo:<br>Special Thanks</h2>
-				<p class="lead">A special thanks to <a target="_blank" href="http://join.deathtothestockphoto.com/">Death to the Stock Photo</a> for providing the photographs that you see in this template. Visit their website to become a member.</p>
-			</div>
-			<div class="col-lg-5 col-lg-offset-2 col-sm-6">
-				<img class="img-responsive" src="<?=base_url()?>frontend-assets/img/ipad.png" alt="">
-			</div>
-		</div>
-
-	</div>
-	<!-- /.container -->
-
-</div>
-<!-- /.content-section-a -->
-
-<div class="content-section-b">
-
-	<div class="container">
-
-		<div class="row">
-			<div class="col-lg-5 col-lg-offset-1 col-sm-push-6  col-sm-6">
-				<hr class="section-heading-spacer">
-				<div class="clearfix"></div>
-				<h2 class="section-heading">3D Device Mockups<br>by PSDCovers</h2>
-				<p class="lead">Turn your 2D designs into high quality, 3D product shots in seconds using free Photoshop actions by <a target="_blank" href="http://www.psdcovers.com/">PSDCovers</a>! Visit their website to download some of their awesome, free photoshop actions!</p>
-			</div>
-			<div class="col-lg-5 col-sm-pull-6  col-sm-6">
-				<img class="img-responsive" src="<?=base_url()?>frontend-assets/img/dog.png" alt="">
-			</div>
-		</div>
-
-	</div>
-	<!-- /.container -->
-
-</div>
-<!-- /.content-section-b -->
-
-<div class="content-section-a">
-
-	<div class="container">
-
-		<div class="row">
-			<div class="col-lg-5 col-sm-6">
-				<hr class="section-heading-spacer">
-				<div class="clearfix"></div>
-				<h2 class="section-heading">Google Web Fonts and<br>Font Awesome Icons</h2>
-				<p class="lead">This template features the 'Lato' font, part of the <a target="_blank" href="http://www.google.com/fonts">Google Web Font library</a>, as well as <a target="_blank" href="http://fontawesome.io">icons from Font Awesome</a>.</p>
-			</div>
-			<div class="col-lg-5 col-lg-offset-2 col-sm-6">
-				<img class="img-responsive" src="<?=base_url()?>frontend-assets/img/phones.png" alt="">
-			</div>
-		</div>
-
-	</div>
-	<!-- /.container -->
-
-</div>
-<!-- /.content-section-a -->
-
-<a  name="contact"></a>
-<div class="banner">
-
-	<div class="container">
-
-		<div class="row">
-			<div class="col-lg-6">
-				<h2>Connect to Start Bootstrap:</h2>
-			</div>
-			<div class="col-lg-6">
-				<ul class="list-inline banner-social-buttons">
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav navbar-right">
 					<li>
-						<a href="https://twitter.com/SBootstrap" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
+						<a href="#about">Home</a>
 					</li>
-					<li>
-						<a href="https://github.com/IronSummitMedia/startbootstrap" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
-					</li>
-					<li>
-						<a href="#" class="btn btn-default btn-lg"><i class="fa fa-linkedin fa-fw"></i> <span class="network-name">Linkedin</span></a>
-					</li>
-				</ul>
-			</div>
-		</div>
-
-	</div>
-	<!-- /.container -->
-
-</div>
-<!-- /.banner -->
-
-<!-- Footer -->
-<footer>
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-12">
-				<ul class="list-inline">
-					<li>
-						<a href="#">Home</a>
-					</li>
-					<li class="footer-menu-divider">&sdot;</li>
 					<li>
 						<a href="#about">About</a>
 					</li>
-					<li class="footer-menu-divider">&sdot;</li>
 					<li>
-						<a href="#services">Services</a>
+						<a href="#services">Feature</a>
 					</li>
-					<li class="footer-menu-divider">&sdot;</li>
 					<li>
-						<a href="#contact">Contact</a>
+						<a href="#contact">FAQ</a>
+					</li>
+					<li>
+						<a href="#whatsnext">What's next?</a>
 					</li>
 				</ul>
-				<p class="copyright text-muted small">Copyright &copy; Your Company 2014. All Rights Reserved</p>
+			</div>
+			<!-- /.navbar-collapse -->
+		</div>
+		<!-- /.container -->
+	</nav>
+
+
+	<!-- Header -->
+	<a name="about"></a>
+	<div class="intro-header">
+		<div class="container">
+
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="intro-message">
+						<h3>Plan your family events easly than ever!</h3>
+						<hr class="intro-divider">
+						<ul class="list-inline intro-social-buttons">
+							<li>
+								<a href="#" class="btn btn-default btn-lg tour-btn"><i class="fa fa-flag fa-fw"></i> <span class="network-name">Take a tour</span></a>
+							</li>
+							<li>
+								<a href="<?=base_url()?>account/login" class="btn btn-default btn-lg get-started-btn"><i class="fa fa-sign-in fa-fw"></i> <span class="network-name">Get started</span></a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+
+		</div>
+		<!-- /.container -->
+
+	</div>
+	<!-- /.intro-header -->
+
+	<!-- Page Content -->
+
+	<a  name="services"></a>
+	<div class="content-section-a">
+
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6 col-lg-offset-1 col-sm-6">
+					<img class="img-responsive ps-img" src="<?=base_url()?>frontend-assets/img/ps.png" alt="">
+				</div>
+				<div class="col-lg-5 col-sm-6 title-head">
+					<div class="clearfix"></div>
+					<h2 class="section-heading ">About Planescape</h2>
+					<p class="lead">Lorem ipsum dolor sit amet, et eam altera convenire liberavisse, cetero molestiae democritum mel cu, et esse quas feugait per. Ad mel phaedrum repudiandae, enim vocent definiebas pro et. Ex mea mazim utamur convenire, ei verterem dissentias quaerendum eos. </p>
+					<p class="lead">Eos et alii deserunt, no utamur nostrum nam. Ex nostro tibique pro, summo vocibus ei usu. Impetus pericula et usu, ius et legere primis sadipscing. Nihil ullamcorper at cum, ut eam ubique delenit ullamcorper.</p>
+				</div>
+			</div>
+
+		</div>
+		<!-- /.container -->
+
+	</div>
+	<!-- /.content-section-a -->
+
+	<div class="content-section-b features-row">
+		<div class="container features-row">
+			<div class="row features-row">
+				<div class="features">Features</div>
+				<div class="feature-col">
+
+				<div class="feature-col1-group">
+					<div class="feature-col1"><i class="fa fa-2x fa-scissors" aria-hidden="true"></i>
+					</div>
+					<div class="feature-col1">Lorem ipsum dolor sit amet adipiscing elit, sed do ciusmod
+					</div>
+				</div>
+
+				<div class="feature-col1-group">
+					<div class="feature-col1"><i class="fa fa-2x fa-folder-open-o" aria-hidden="true"></i>
+					</div>
+					<div class="feature-col1">Lorem ipsum dolor sit amet adipiscing elit, sed do ciusmod
+					</div>
+				</div>
+
+				<div class="feature-col1-group">
+					<div class="feature-col1"><i class="fa fa-2x fa-users" aria-hidden="true"></i>
+
+					</div>
+					<div class="feature-col1">Lorem ipsum dolor sit amet adipiscing elit, sed do ciusmod
+					</div>
+				</div>
+
+				<div class="feature-col1-group">
+					<div class="feature-col1"><i class="fa fa-2x fa-comments-o" aria-hidden="true"></i>
+
+					</div>
+					<div class="feature-col1">Lorem ipsum dolor sit amet adipiscing elit, sed do ciusmod
+					</div>
+				</div>
+
+				<div class="feature-col1-group">
+					<div class="feature-col1"><i class="fa fa-2x fa-paper-plane-o" aria-hidden="true"></i>
+
+					</div>
+					<div class="feature-col1">Lorem ipsum dolor sit amet adipiscing elit, sed do ciusmod
+					</div>
+				</div>
+
+				<div class="feature-col1-group">
+					<div class="feature-col1"><i class="fa fa-2x fa-pencil" aria-hidden="true"></i>
+
+					</div>
+					<div class="feature-col1">Lorem ipsum dolor sit amet adipiscing elit, sed do ciusmod
+					</div>
+				</div>
+
+				</div>
+			</div>
+
+		</div>
+		<!-- /.container -->
+
+	</div>
+	<!-- /.content-section-b -->
+
+	<div class="content-section-a">
+		<div class="container">
+			<div class="row faq-row">
+				<div class="faq">FAQ</div>
+				<div class="faq-col">
+
+				<div class="faq-col1-group">
+					<div class="faq-col1 clear title">1. Qeuestion number one
+					</div>
+					<div class="faq-col1 clear">Lorem ipsum dolor sit amet adipiscing elit, sed do ciusmod
+					</div>
+				</div>
+
+				<div class="faq-col1-group">
+					<div class="faq-col1 clear title">2. Qeuestion number two
+					</div>
+					<div class="faq-col1 clear">Lorem ipsum dolor sit amet adipiscing elit, sed do ciusmod
+					</div>
+				</div>
+
+				<div class="faq-col1-group">
+					<div class="faq-col1 clear title">3. Qeuestion number three
+					</div>
+					<div class="faq-col1 clear">Lorem ipsum dolor sit amet adipiscing elit, sed do ciusmod
+					</div>
+				</div>
+
+				<div class="faq-col1-group">
+					<div class="faq-col1 clear title">4. Qeuestion number four
+					</div>
+					<div class="faq-col1 clear">Lorem ipsum dolor sit amet adipiscing elit, sed do ciusmod
+					</div>
+				</div>
+
+				
+
+				</div>
+			</div>
+
+		</div>
+		<!-- /.container -->
+
+	</div>
+	<!-- /.content-section-a -->
+
+	<!-- /.content-section-a -->
+
+	<div class="content-section-b whatsnext-row">
+		<div class="container whatsnext-row">
+			<div class="row whatsnext-row">
+				<div class="whatsnext">What's next</div>
+				<div class="whatsnext-col">
+
+				<div class="whatsnext-col1-group">
+					<p class="lead">Lorem ipsum dolor sit amet, et eam altera convenire liberavisse, cetero molestiae democritum mel cu, et esse quas feugait per. Ad mel phaedrum repudiandae, enim vocent definiebas pro et. Ex mea mazim utamur convenire, ei verterem dissentias quaerendum eos.
+					Eos et alii deserunt, no utamur nostrum nam. Ex nostro tibique pro, summo vocibus ei usu. Impetus pericula et usu, ius et legere primis sadipscing. Nihil ullamcorper at cum, ut eam ubique delenit ullamcorper.
+					</p>
+					<p class="lead">
+					Lorem ipsum dolor sit amet, et eam altera convenire liberavisse, cetero molestiae democritum mel cu, et esse quas feugait per. Ad mel phaedrum repudiandae, enim vocent definiebas pro et. Ex mea mazim utamur convenire, ei verterem dissentias quaerendum eos.
+					Eos et alii deserunt, no utamur nostrum nam. Ex nostro tibique pro, summo vocibus ei usu. Impetus pericula et usu, ius et legere primis sadipscing. Nihil ullamcorper at cum, ut eam ubique delenit ullamcorper.</p>
+				</div>
+
+				
+
+				</div>
+			</div>
+
+		</div>
+		<!-- /.container -->
+
+	</div>
+	<!-- /.content-section-b -->
+
+	<!-- Footer -->
+	<footer class="footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<p class="copyright text-muted small">Copyright &copy; by PlaneScape 2016. Allrights reserved.</p>
+				</div>
 			</div>
 		</div>
-	</div>
-</footer>
+	</footer>
 
-<!-- jQuery -->
-<script src="<?=base_url()?>frontend-assets/js/jquery.js"></script>
+	<!-- jQuery -->
+	<script src="<?=base_url()?>frontend-assets/js/jquery.js"></script>
 
-<!-- Bootstrap Core JavaScript -->
-<script src="<?=base_url()?>frontend-assets/js/bootstrap.min.js"></script>
+	<!-- Bootstrap Core JavaScript -->
+	<script src="<?=base_url()?>frontend-assets/js/bootstrap.min.js"></script>
 
 </body>
 </html>
